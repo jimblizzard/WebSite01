f2d27a4b3e3e10b5862fdea03d81add9e8bfc1ef
Jim Blizzard <jimblizzard@hotmail.com>
2015-03-26 08:55:07 -0400

    initial

---
+++ blizWeb01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="blizWeb01.MvcApplication" Language="C#" %>
---
+++ blizWeb01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(blizWeb01.Startup))]
+namespace blizWeb01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
